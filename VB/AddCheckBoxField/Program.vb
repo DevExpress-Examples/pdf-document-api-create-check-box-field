4025815d6eff57485ba88d939ec0f4fddd81f6ad
DevExpressExampleBot <fakeEmail>
2022-02-02 15:14:57 +0300

    Source auto update [skip ci]

--- VB/AddCheckBoxField/Program.vb
+++ VB/AddCheckBoxField/Program.vb
@@ -1,18 +1,17 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports System.Drawing
 
 Namespace AddCheckBoxField
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
-            Using processor As New PdfDocumentProcessor()
 
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Create an empty document. 
                 processor.CreateEmptyDocument("..\..\Result.pdf")
-
                 ' Create graphics and draw a check box field.
                 Using graphics As PdfGraphics = processor.CreateGraphics()
                     DrawCheckBoxField(graphics)
-
                     ' Render a page with graphics.
                     processor.RenderNewPage(PdfPaperSize.Letter, graphics)
                 End Using
@@ -20,16 +19,13 @@ Namespace AddCheckBoxField
         End Sub
 
         Private Shared Sub DrawCheckBoxField(ByVal graphics As PdfGraphics)
-
             ' Create a check box field specifying its name and location.
-            Dim checkBox As New PdfGraphicsAcroFormCheckBoxField("check box", New RectangleF(20, 20, 30, 30))
-
+            Dim checkBox As PdfGraphicsAcroFormCheckBoxField = New PdfGraphicsAcroFormCheckBoxField("check box", New RectangleF(20, 20, 30, 30))
             ' Specify check box appearance, checked state, and button style.
             checkBox.Appearance.BackgroundColor = Color.Azure
             checkBox.Appearance.BorderAppearance = New PdfGraphicsAcroFormBorderAppearance() With {.Color = Color.Red}
             checkBox.IsChecked = True
             checkBox.ButtonStyle = PdfAcroFormButtonStyle.Star
-
             ' Add the field to the document.
             graphics.AddFormField(checkBox)
         End Sub
